7cb518b0455db073eae5d21993c0648c66f2781f
Clothkeda <clothkeda@gmail.com>
2026-07-01 14:04:45 +0800

    upload new unity project

---
+++ TextGame project/Assets/Scripts/VNManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class VNManager : MonoBehaviour
+{
+    public Text speakerName;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
