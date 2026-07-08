560358fda133d5ccf97014ed9b94ef80bee3789f
a2219544480 <2219544480@qq.com>
2026-07-08 16:50:07 +0800

    V8.0

--- TextGame project/Assets/Scripts/PlayerController.cs
+++ TextGame project/Assets/Scripts/PlayerController.cs
@@ -4,30 +4,40 @@ public class PlayerController : MonoBehaviour
 {
     private Animator ani;
     private Rigidbody2D rBody;
+    // 保存上一帧有效移动方向
+    private float lastH;
+    private float lastV;
+    
     void Start()
     {
         ani = GetComponent<Animator>();
         rBody = GetComponent<Rigidbody2D>();
     }
 
-    
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 dir = Vector2.zero;
+
+        // 单轴互斥逻辑：左右优先
         if (horizontal != 0)
         {
-            ani.SetFloat("Horizontal", horizontal);
-            ani.SetFloat("Vertical", 0);
+            dir = new Vector2(horizontal, 0);
+            lastH = horizontal;
+            lastV = 0;
         }
-
-        if (vertical != 0)
+        else if (vertical != 0)
         {
-            ani.SetFloat("Horizontal", 0);
-            ani.SetFloat("Vertical", vertical);
+            dir = new Vector2(0, vertical);
+            lastH = 0;
+            lastV = vertical;
         }
-        Vector2 dir = new Vector2(horizontal, vertical);
+        
+        ani.SetFloat("Horizontal", lastH);
+        ani.SetFloat("Vertical", lastV);
         ani.SetFloat("Speed", dir.magnitude);
+        
         rBody.linearVelocity = dir * 2f;
     }
 }
